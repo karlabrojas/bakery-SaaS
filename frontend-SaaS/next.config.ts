e35551b6bd3e3ed40f655e09ca2642cf9ecab181
Karlabrojas <karlabrojasr2@gmail.com>
2026-07-05 19:34:05 -0600

    fix: corrige next.config

--- frontend-SaaS/next.config.ts
+++ frontend-SaaS/next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
+const supabaseHostname = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL!)
+  .hostname;
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:
-          process.env.SUPABASE_URL?.replace("https://", "").replace("/", "") ||
-          "",
+        hostname: supabaseHostname,
       },
     ],
   },
